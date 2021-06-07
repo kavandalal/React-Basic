d9446a6196fd9b20de7b3537219980afa562aead
kavandalal003@gmail.com <kavandalal003@gmail.com>
2021-06-07 22:12:00 +0530

    1st try

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import './style.css' ;
+
+function BookList(){
+  return(
+    <section className='booklistclass'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
-export default App;
+function Book(){
+  return(
+    <article className="bookclass">
+      <img src="https://i.pinimg.com/originals/c4/45/0a/c4450aa5a77c5354023e2a50d76b46c5.png" alt=''/>
+      <p>
+        Champion
+      </p>
+      <h3>
+        Sylas : The Unshackled 
+      </h3>
+    </article>
+  );
+}
+
+export default BookList;
