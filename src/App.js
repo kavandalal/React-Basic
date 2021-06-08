0af0d3642e839083696d13bb55a23a639bee3c15
kavandalal003@gmail.com <kavandalal003@gmail.com>
2021-06-08 13:34:10 +0530

    2nd Update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
 import React from 'react';
-
+//css
 import './style.css' ;
+
+//dataset
+import {pokemons} from './data/data';
 
 function BookList(){
   return(
     <section className='booklistclass'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {pokemons.map((pokemon) => {
+        // return <Book pokemon ={pokemon}></Book>; //method 1 
+        return <Book key={pokemon.ind} {...pokemon}> </Book>; //method2 key for the warning in the console
+      })};
+      
     </section>
   );
 }
 
-function Book(){
+function Book(props){
+  // const {img,pname,type,ht,wt} = props.pokemon; // method 1 
+  const {img,pname,type,ht,wt} = props; //method 2 
   return(
     <article className="bookclass">
-      <img src="https://i.pinimg.com/originals/c4/45/0a/c4450aa5a77c5354023e2a50d76b46c5.png" alt=''/>
-      <p>
-        Champion
-      </p>
-      <h3>
-        Sylas : The Unshackled 
-      </h3>
+      <img src={img} alt=''/>
+      <div>
+        <h1>{pname}</h1>
+        {ColorType(type)}
+      </div>
+      <div style = {{"style" : 'inline'}}>
+        <h5 style = {{"style" : 'inline','float':'left'}}>Height {ht}</h5>
+        <h5 style = {{"style" : 'inline','float':'right'}}>Weight {wt}</h5>
+      </div>
     </article>
   );
+}
+
+const ColorType=(type)=>{
+  if(type === 'Grass') return <h4 class="grasscolor">{type}</h4>;
+  else if(type === 'Fire') return <h4 class="firecolor">{type}</h4>;
+  else if(type === 'Water') return <h4 class="watercolor">{type}</h4>;
 }
 
 export default BookList;
---
+++ src/data/data.js
@@ -0,0 +1,119 @@
+export const pokemons = [
+ {
+   "ind": 1,
+   "img":"https://i.imgur.com/3o9cfNW.jpg",
+   "pokedex_number": 1,
+   "pname": "Bulbasaur",
+   "type": "Grass",
+   "ht": 0.7,
+   "wt": 6.9
+ },
+ {
+   "ind": 2,
+   "img":"https://i.imgur.com/LvpVbVK.jpg",
+   "pokedex_number": 2,
+   "pname": "Ivysaur",
+   "type": "Grass",
+   "ht": 1,
+   "wt": 13
+ },
+ {
+   "ind": 3,
+   "img":"https://thenordicweb.com/image/https%3A%2F%2Fassets.pokemon.com%2Fassets%2Fcms2%2Fimg%2Fpokedex%2Ffull%2F003.png?table=block&id=7b5bde87-6259-4692-97db-0c9b751110df&width=250&userId=&cache=v2",
+   "pokedex_number": 3,
+   "pname": "Venusaur",
+   "type": "Grass",
+   "ht": 2,
+   "wt": 100
+ },
+ {
+   "ind": 4,
+   "img":"http://media-cerulean.cursecdn.com/attachments/5/860/mega_venusaur.png",
+   "pokedex_number": 3,
+   "pname": "Mega Venusaur",
+   "type": "Grass",
+   "ht": 2.4,
+   "wt": 155.5
+ },
+ {
+   "ind": 5,
+   "img":"https://cdn2.bulbagarden.net/upload/thumb/7/73/004Charmander.png/250px-004Charmander.png",
+   "pokedex_number": 4,
+   "pname": "Charmander",
+   "type": "Fire",
+   "ht": 0.6,
+   "wt": 8.5
+ },
+ {
+   "ind": 6,
+   "img":"https://i.imgur.com/pkjbZMP.jpg",
+   "pokedex_number": 5,
+   "pname": "Charmeleon",
+   "type": "Fire",
+   "ht": 1.1,
+   "wt": 19
+ },
+ {
+   "ind": 7,
+   "img":"https://qph.fs.quoracdn.net/main-qimg-826946bec5fbef866b6dd2f3a7be4ce4",
+   "pokedex_number": 6,
+   "pname": "Charizard",
+   "type": "Fire",
+   "ht": 1.7,
+   "wt": 90.5
+ },
+ {
+   "ind": 8,
+   "img":"https://sg.portal-pokemon.com/play/resources/pokedex/img/pm/ca3db4aad5c85a525d9be86852b26db1db7a22c0.png",
+   "pokedex_number": 6,
+   "pname": "Mega Charizard X",
+   "type": "Fire",
+   "ht": 1.7,
+   "wt": 110.5
+ },
+ {
+   "ind": 9,
+   "img":"https://thenordicweb.com/image/https%3A%2F%2Fassets.pokemon.com%2Fassets%2Fcms2%2Fimg%2Fpokedex%2Ffull%2F006_f3.png?table=block&id=513631dd-14a9-44bf-82f2-f72f4789447c&width=250&userId=&cache=v2",
+   "pokedex_number": 6,
+   "pname": "Mega Charizard Y",
+   "type": "Fire",
+   "ht": 1.7,
+   "wt": 100.5
+ },
+ {
+   "ind": 10,
+   "img":"https://i.imgur.com/nK9caY2.jpg",
+   "pokedex_number": 7,
+   "pname": "Squirtle",
+   "type": "Water",
+   "ht": 0.5,
+   "wt": 9
+ },
+ {
+   "ind": 11,
+   "img":"https://thenordicweb.com/image/https%3A%2F%2Fassets.pokemon.com%2Fassets%2Fcms2%2Fimg%2Fpokedex%2Ffull%2F008.png?table=block&id=babbd5f9-44a1-48a0-a785-dbeea6a58b8e&width=250&userId=&cache=v2",
+   "pokedex_number": 8,
+   "pname": "Wartortle",
+   "type": "Water",
+   "ht": 1,
+   "wt": 22.5
+ },
+ {
+   "ind": 12,
+   "img":"https://static.pokemonpets.com/images/PokemonAssets/250px-009Blastoise.png",
+   "pokedex_number": 9,
+   "pname": "Blastoise",
+   "type": "Water",
+   "ht": 1.6,
+   "wt": 85.5
+ },
+ {
+   "ind": 13,
+   "img":"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f5ab561c-405b-4649-86b2-096272709cb7/d7jt663-c29ee0c0-c3c9-4882-a604-b3bd14d54e0f.png/v1/fill/w_250,h_250,strp/pokemon_009m___mega_blastoise_by_illustrationoverdose_d7jt663-250t.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODAwIiwicGF0aCI6IlwvZlwvZjVhYjU2MWMtNDA1Yi00NjQ5LTg2YjItMDk2MjcyNzA5Y2I3XC9kN2p0NjYzLWMyOWVlMGMwLWMzYzktNDg4Mi1hNjA0LWIzYmQxNGQ1NGUwZi5wbmciLCJ3aWR0aCI6Ijw9ODAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.kYrgMbOUmoSZ1rDdQwrOPCb5cYJj7GRzPf8AD1Qc65w",
+   "pokedex_number": 9,
+   "pname": "Mega Blastoise",
+   "type": "Water",
+   "ht": 1.6,
+   "wt": 101.1
+ }
+]
